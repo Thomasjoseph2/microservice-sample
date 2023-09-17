f12e7c30d2c5f7b8007fa0be1e376262d0e57cce
Thomasjoseph2 <tj089502@gmail.com>
2023-09-17 11:23:39 +0530

    deployment started

--- event-bus/index.js
+++ event-bus/index.js
@@ -12,25 +12,25 @@ app.post("/events", (req, res) => {
 
   events.push(event);
 
-  axios.post("http://localhost:4000/events", event).catch((err) => {
+  axios.post("http://posts-clusterip-srv:4000/events", event).catch((err) => {
     console.log(err.message);
   });
-  axios.post("http://localhost:4001/events", event).catch((err) => {
-    console.log(err.message);
-  });
-  axios.post("http://localhost:4002/events", event).catch((err) => {
-    console.log(err.message);
-  });
-  axios.post("http://localhost:4003/events", event).catch((err) => {
-    console.log(err.message);
-  });
-  res.send({ status: "OK" });
+  // axios.post("http://localhost:4001/events", event).catch((err) => {
+  //   console.log(err.message);
+  // });
+  // axios.post("http://localhost:4002/events", event).catch((err) => {
+  //   console.log(err.message);
+  // });
+  // axios.post("http://localhost:4003/events", event).catch((err) => {
+  //   console.log(err.message);
+  // });
+  // res.send({ status: "OK" });
 });
 
 app.get("/events", (req, res) => {
   res.send(events);
 });
-
+ 
 app.listen(4005, () => {
   console.log("Listening on 4005");
 });
